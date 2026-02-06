7ff69936c49c4d6973a8cec1a9392ece59f4c79b
Fawad Mahmood <fawad@washmen.com>
2026-02-06 20:41:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const probot = new Probot();
 
 require('./handlers')(probot);
 
+// Add a simple health check route
+probot.server.get('/', (req, res) => {
+  res.send('GitHub App is running and ready to receive webhooks.');
+});
+
 module.exports = probot.server;
