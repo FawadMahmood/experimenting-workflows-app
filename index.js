ff3e31c20c7e7e553345633d5b74f6e115508886
Fawad Mahmood <fawad@washmen.com>
2026-02-06 21:58:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { Probot } from 'probot';
 import handlers from './handlers.js';
 
+console.log('APP_ID:', process.env.APP_ID);
+console.log('PRIVATE_KEY length:', process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY.length : 'undefined');
+console.log('WEBHOOK_SECRET length:', process.env.WEBHOOK_SECRET ? process.env.WEBHOOK_SECRET.length : 'undefined');
+
 if (!process.env.APP_ID || !process.env.PRIVATE_KEY || !process.env.WEBHOOK_SECRET) {
   throw new Error('Missing required environment variables: APP_ID, PRIVATE_KEY, WEBHOOK_SECRET');
 }
@@ -14,6 +18,10 @@ try {
   });
 } catch (error) {
   throw new Error('Failed to create Probot instance: ' + error.message);
+}
+
+if (!probot || !probot.server) {
+  throw new Error('Probot instance or server not created properly');
 }
 
 handlers(probot);
